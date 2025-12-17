bf3132d3708245e40d7d05e9a7cb6f9b752c214c
yogiprasaktikuliah <yogiprasaktikuliah@gmail.com>
2025-12-18 02:41:16 +0800

    Add files via upload

---
+++ styles/Kelurahan_Sei_Andai_2_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_Kelurahan_Sei_Andai_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "10.4px \'MS Shell Dlg 2\', sans-serif";
+    var labelFill = "#ffffff";
+    var bufferColor = "#000000";
+    var bufferWidth = 1.5000000000000004;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("NAMOBJ") !== null) {
+        labelText = String(feature.get("NAMOBJ"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(152,125,183,0.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
